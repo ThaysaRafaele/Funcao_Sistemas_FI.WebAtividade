b63490da34d0872c5e9fb866823181fbd8a49003
Thaysa Lacerda <t_thaysa.lacerda@hapvida.com.br>
2024-10-28 01:00:23 -0400

    feat: ajuste validação do cpf

--- FI.WebAtividadeEntrevista/Scripts/Clientes/FI.AltClientes.js
+++ FI.WebAtividadeEntrevista/Scripts/Clientes/FI.AltClientes.js
@@ -16,6 +16,12 @@ $(document).ready(function () {
     $('#formCadastro').submit(function (e) {
         e.preventDefault();
 
+        var cpf = $('#formCadastro #CPF').val();
+        if (!validaCPF(cpf)) {
+            ModalDialog("Erro", "CPF inválido");
+            return false;
+        }
+
         $.ajax({
             url: urlPost,
             method: "POST",
@@ -29,7 +35,7 @@ $(document).ready(function () {
                 "Cidade": $(this).find("#Cidade").val(),
                 "Logradouro": $(this).find("#Logradouro").val(),
                 "Telefone": $(this).find("#Telefone").val(),
-                "CPF": $(this).find("#CPF").val()
+                "CPF": cpf
             },
             error:
                 function (r) {
@@ -91,6 +97,46 @@ $(document).ready(function () {
 
 
 })
+
+
+function validaCPF(cpf) {
+    cpf = cpf.replace(/[^\d]/g, '');
+
+    if (cpf.length !== 11) {
+        return false;
+    }
+
+    if (/^(\d)\1{10}$/.test(cpf)) {
+        return false;
+    }
+
+    let soma = 0;
+    for (let i = 0; i < 9; i++) {
+        soma += parseInt(cpf.charAt(i)) * (10 - i);
+    }
+    let resto = (soma * 10) % 11;
+    if (resto === 10 || resto === 11) {
+        resto = 0;
+    }
+    if (resto !== parseInt(cpf.charAt(9))) {
+        return false;
+    }
+
+    soma = 0;
+    for (let i = 0; i < 10; i++) {
+        soma += parseInt(cpf.charAt(i)) * (11 - i);
+    }
+    resto = (soma * 10) % 11;
+    if (resto === 10 || resto === 11) {
+        resto = 0;
+    }
+    if (resto !== parseInt(cpf.charAt(10))) {
+        return false;
+    }
+
+    return true;
+}
+
 
 function editarBeneficiario(id) {
     $.ajax({
